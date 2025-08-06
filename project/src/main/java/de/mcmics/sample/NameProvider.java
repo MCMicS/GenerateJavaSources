d33467375bf841564bcd3373d65d38610a2a5a43
mcmics <MCMicS@users.noreply.github.com>
2025-08-06 21:55:05 +0200

    only 100 classes

--- project/src/main/java/de/mcmics/sample/NameProvider.java
+++ project/src/main/java/de/mcmics/sample/NameProvider.java
@@ -2,7 +2,7 @@ package de.mcmics.sample;
 
 import de.mcmics.annotation.NameCreator;
 
-@NameCreator(number = 10_000)
+@NameCreator(number = 100)
 public interface NameProvider {
     String getName();
 }
